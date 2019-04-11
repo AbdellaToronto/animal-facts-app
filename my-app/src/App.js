32403515e70f55f267acd1c856bf1710c702c528
Jaymie Rosen <jaymie.rosen@publicisna.com>
2019-04-11 14:36:33 -0400

    Creating components folder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from './Card';
+import Card from './Components/Card';
 
 class App extends Component {
   constructor(props) {
