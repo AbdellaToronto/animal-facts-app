48c7ee37f0e6e0313baa5c2cf15844b135a1b915
jaymierosen <rosen.jaymie@gmail.com>
2019-06-27 19:19:30 -0400

    going to revert back to an earlier version of app

--- src/App.js
+++ src/App.js
@@ -34,12 +34,8 @@ class App extends Component {
   /* *** amount is a parameter for this.state.numberOfFacts*/
   getAnimalFact = (animalType = 'dog', amount = 1) => {
     fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=${animalType}&amount=${amount}`)
+    .then(data => data.json())
     .then(data => {
-      debugger;
-      return data.json();
-    })
-    .then(data => {
-      debugger;
       this.setState({
         /* So what's happening here is that I'm expecting facts to always be an array, so first i check to see if DATA is an array - if it is, then I'm assuming that it's an array of fact objects, get the text property off of each and put them in that new array, however if data is NOT an array, well before I assign it to facts, I need to make sure it's structured as an array, so I take the text property off of the fact and put it in an arra. Another way to say it is 'if data is an array of facts, get the text of each fact and put it into a new array, and set that to my 'facts' state. However if it's not an array and JUST a single fact object, take the text off of it, and put that in a new array.That way no matter what, facts state will ALWAYS be an array. And saying this.state.facts.map in my jsx will never be an issue */
         facts: Array.isArray(data) ? data.map(fact => fact.text) : [data.text]
