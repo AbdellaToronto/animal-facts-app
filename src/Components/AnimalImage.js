e19f07bcc00b2b428bc60d6d26aa0079e19f4def
AbdellaToronto <abdellaali@Abdellas-MacBook-Pro.local>
2019-07-16 13:32:44 -0400

    tweak things

--- src/Components/AnimalImage.js
+++ src/Components/AnimalImage.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import dog from '../images/dog.jpg';
-import cat from '../images/cat.jpg';
 
 export const AnimalImage = (animalType) => {
     return ( 
         <div> 
-            {animalType.animalType === 'dog' ? <img className="animal" alt={animalType.animalType} src ={cat} /> : <img className="animal" alt={animalType.animalType} src={dog} /> }
+            <img className="animal" alt={animalType.animalType} src ={require(`../images/${animalType.animalType}.jpg`)} />
         </div>
     );
 };
